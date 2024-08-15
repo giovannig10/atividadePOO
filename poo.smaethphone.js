4febe4043400651566d29c77c4e078ea24a80f75
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:59:45 -0300

    smarthphone

---
+++ poo.smaethphone.js
@@ -0,0 +1,6 @@
+const smarthphone = {
+    marca: "apple",
+    modelo: "iphone 3s",
+    anoLancamento: "2009"
+}
+console.log(" marca: " + smarthphone.marca + ", " + "modelo: " + smarthphone.modelo + ", " + "ano de lancamento: " + smarthphone.anoLancamento );
