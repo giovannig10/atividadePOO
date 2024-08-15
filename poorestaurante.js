e2293bd3a676740c188a51608dbe0dad0777074e
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:49:56 -0300

    restaurante

---
+++ poorestaurante.js
@@ -0,0 +1,6 @@
+const restaurante = {
+    nome: "scoobynho",
+    tipoCozinha: "lanches",
+    localizacao: "campinas"
+}
+console.log(" nome: " + restaurante.nome + ", " + "tipo de cozinha: " + restaurante.tipoCozinha + ", " + "localização: " + restaurante.localizacao );
