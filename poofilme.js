d86a1a82717c7ab62460f73443087842a81227f1
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:37:02 -0300

    filme

---
+++ poofilme.js
@@ -0,0 +1,6 @@
+const filme = {
+    titulo: "Velozes e Furiosos",
+    diretor: "vin diesel",
+    anoLancamento: "2001"
+}
+console.log(" titulo: " + filme.titulo + ", " + "diretor: " + filme.diretor + ", " + "ano de lancamento: " + filme.anoLancamento );
