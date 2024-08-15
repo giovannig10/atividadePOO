267c1e5478aec8f82283bd39c6f43b8d658d41f7
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:43:03 -0300

    musica

---
+++ poomusica.js
@@ -0,0 +1,6 @@
+const musica = {
+    titulo: "ilhabela 2",
+    artista: "mc magal",
+    album: false
+}
+console.log(" titulo: " + musica.titulo + ", " + "artista: " + musica.artista + ", " + "album: " + musica.album );
