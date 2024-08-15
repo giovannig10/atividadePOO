c59476f4173bd8700cdc8131f4072cdd37e64120
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:54:22 -0300

    jogo de videogame

---
+++ poojogo.js
@@ -0,0 +1,6 @@
+const jogo = {
+    titulo: "EAFC24",
+    desenvolvedor: "EA sports",
+    plataforma: "Variado"
+}
+console.log(" titulo: " + jogo.titulo + ", " + "desenvolvedor: " + jogo.desenvolvedor + ", " + "plataforma: " + jogo.plataforma );
