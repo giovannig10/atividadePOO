abd7b6c98748b76fc03308687935e7b3f613deda
Giovanni Gomes <giovanni.ggoncalves@outlook.com>
2024-08-15 09:29:46 -0300

    livro

--- pooexercicio.js
+++ pooexercicio.js
@@ -1,3 +1,6 @@
 const livro = {
-    titulo: "harry potter"
+    titulo: "harry potter",
+    autor: "J.K Rowling",
+    ano: "1997"
 }
+console.log(" titulo " + livro.titulo + ", " + "autor " + livro.autor + ", " + "ano " + livro.ano );
